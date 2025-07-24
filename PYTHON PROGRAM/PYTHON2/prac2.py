5e4543bd132af20dc6dc91bdfdcf917857e422ea
shlok pandey <pandeyjishlok@gmail.com>
2025-07-24 17:12:11 +0530

    Add files via upload

---
+++ PYTHON PROGRAM/PYTHON2/prac2.py
@@ -0,0 +1,8 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========= RESTART: C:/Users/PC25/Desktop/PYTHON PRACTICAL/Practical1.py ========
+Enter your first number:85
+Enter your second number:65
+Enter your third number:25
+a is greater
